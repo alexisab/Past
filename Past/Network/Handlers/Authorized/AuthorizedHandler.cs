b443f358038ad8e08ff709f3a9e0ec89702d9536
skeezr <skeeezr@gmail.com>
2016-09-09 16:45:17 +0200

    ...

--- Past/Network/Handlers/Authorized/AuthorizedHandler.cs
+++ Past/Network/Handlers/Authorized/AuthorizedHandler.cs
@@ -7,7 +7,7 @@ namespace Past.Network.Handlers.Authorized
     {
         public static void HandleAdminQuietCommandMessage(GameClient client, AdminQuietCommandMessage message)
         {
-            client.Send(new ChatServerMessage(1, message.content, 0, "", client.Character.Id, client.Character.Name);
+            client.Send(new ChatServerMessage(1, message.content, 0, "", client.Character.Id, client.Character.Name));
         }
     }
 }
