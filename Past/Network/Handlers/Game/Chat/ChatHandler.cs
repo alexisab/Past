90c4c364f97579bf52301f5d6b21fbd4c5d25838
skeezr <skeeezr@gmail.com>
2016-09-17 00:07:23 +0200

    start to implement the chat

--- Past/Network/Handlers/Game/Chat/ChatHandler.cs
+++ Past/Network/Handlers/Game/Chat/ChatHandler.cs
@@ -1,10 +1,18 @@
 ﻿using Past.Network.Game;
 using Past.Protocol.Messages;
+using Past.Utils;
+using System;
 
 namespace Past.Network.Handlers.Game.Chat
 {
     public class ChatHandler
     {
+
+        public static void HandleChatClientMultiMessage(GameClient client, ChatClientMultiMessage message)
+        {
+            client.Character.Map.CurrentMap.Send(new ChatServerMessage(message.channel, message.content, Functions.ReturnUnixTimeStamp(DateTime.Now), "", client.Character.Id, client.Character.Name));
+        }
+
         public static void HandleChatSmileyRequestMessage(GameClient client, ChatSmileyRequestMessage message)
         {
             client.Character.Map.CurrentMap.Send(new ChatSmileyMessage(client.Character.Id, message.smileyId));
