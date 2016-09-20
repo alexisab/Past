32e4d0c3214a26af603ed35881884c24350d4110
skeezr <skeeezr@gmail.com>
2016-09-20 21:02:44 +0200

    handle private message

--- Past/Network/Handlers/Game/Chat/ChatHandler.cs
+++ Past/Network/Handlers/Game/Chat/ChatHandler.cs
@@ -1,7 +1,9 @@
 ﻿using Past.Network.Game;
+using Past.Protocol.Enums;
 using Past.Protocol.Messages;
 using Past.Utils;
 using System;
+using System.Linq;
 
 namespace Past.Network.Handlers.Game.Chat
 {
@@ -10,14 +12,32 @@ namespace Past.Network.Handlers.Game.Chat
 
         public static void HandleChatClientMultiMessage(GameClient client, ChatClientMultiMessage message)
         {
-            if (message.channel >= 0 && message.channel <= 11)
-                client.Character.Map.CurrentMap.Send(new ChatServerMessage(message.channel, message.content, Functions.ReturnUnixTimeStamp(DateTime.Now), "", client.Character.Id, client.Character.Name));
+            SendChatServerMessage(client, message.channel, message.content, client.Character.Id, client.Character.Name);
         }
 
         public static void HandleChatSmileyRequestMessage(GameClient client, ChatSmileyRequestMessage message)
         {
             if (message.smileyId >= 0 && message.smileyId <= 24)
                 client.Character.Map.CurrentMap.Send(new ChatSmileyMessage(client.Character.Id, message.smileyId));
+        }
+
+        public static void HandleChatClientPrivateMessage(GameClient client, ChatClientPrivateMessage message)
+        {
+            var targetClient = GameServer.Clients.FirstOrDefault(x => x.Character.Name == message.receiver);
+            if (!string.IsNullOrEmpty(message.content))
+            {
+                if (targetClient != null && targetClient != client)
+                    targetClient.Send(new ChatServerMessage((sbyte)ChatActivableChannelsEnum.PSEUDO_CHANNEL_PRIVATE, message.content, Functions.ReturnUnixTimeStamp(DateTime.Now), "", client.Character.Id, client.Character.Name));
+            }
+        }
+
+        public static void SendChatServerMessage(GameClient client, sbyte channel, string content, int senderId, string senderName)
+        {
+            if (!string.IsNullOrEmpty(content))
+            {
+                if (channel >= 0 && channel <= 11)
+                    client.Character.Map.CurrentMap.Send(new ChatServerMessage(channel, content, Functions.ReturnUnixTimeStamp(DateTime.Now), "", senderId, senderName));
+            }
         }
     }
 }
