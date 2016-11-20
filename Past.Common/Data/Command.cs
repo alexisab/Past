421711b8a862433f0f08211781531db9a298b7b1
skeezr <skeeezr@hotmail.com>
2016-11-20 06:52:43 +0100

    commands

---
+++ Past.Common/Data/Command.cs
@@ -0,0 +1,29 @@
+﻿using Past.Protocol.Enums;
+using System.Collections.Generic;
+
+namespace Past.Common.Data
+{
+    public class Command
+    {
+        public GameHierarchyEnum Role { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public static Dictionary<int, Command> Commands = new Dictionary<int, Command>();
+
+        public Command(GameHierarchyEnum role, string name, string description)
+        {
+            Role = role;
+            Name = name;
+            Description = description;
+        }
+
+        public static void Initialize()
+        {
+            Commands.Add(0, new Command(GameHierarchyEnum.PLAYER, "help", "display all commands available"));
+            Commands.Add(1, new Command(GameHierarchyEnum.PLAYER, "save", "save the current character"));
+            Commands.Add(2, new Command(GameHierarchyEnum.PLAYER, "start", "teleport the current character to his start map"));
+            Commands.Add(3, new Command(GameHierarchyEnum.MODERATOR, "goname", "teleport the current character to the target"));
+            Commands.Add(4, new Command(GameHierarchyEnum.GAMEMASTER_PADAWAN, "levelup", "levelup the current character"));
+        }
+    }
+}
--- Past.Game/Engine/CommandEngine.cs
+++ Past.Game/Engine/CommandEngine.cs
@@ -1,52 +1,50 @@
-﻿using Past.Game.Network;
+﻿using Past.Common.Data;
+using Past.Game.Network;
 using Past.Game.Network.Handlers.Basic;
 using Past.Protocol.Enums;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Past.Game.Engine
 {
     public class CommandEngine
     {
-        private static Dictionary<string, string> Commands = new Dictionary<string, string>()
-        {
-            { "help", "display all commands available" },
-            { "save", "save your character" },
-            { "goname", "teleport you to the target" },
-            { "start", "teleport to your start map" },
-            { "levelup", "level up your character" },
-        };
-
+        
         public static void Handle(Client client, string content)
         {
             string[] command = content.Split(' ');
             switch (command[0])
             {
                 case ".help":
-                    foreach (var cmd in Commands)
+                    foreach (var cmd in Command.Commands.Where(@cmd => @cmd.Value.Role <= client.Account.Role))
                     {
-                        BasicHandler.SendTextInformationMessage(client, TextInformationTypeEnum.TEXT_INFORMATION_ERROR, 16, new string[] { $"{cmd.Key}", $"{cmd.Value}." });
+                        BasicHandler.SendTextInformationMessage(client, TextInformationTypeEnum.TEXT_INFORMATION_ERROR, 16, new string[] { $"{cmd.Value.Name}", $"{cmd.Value.Description}." });
                     }
                     break;
                 case ".save":
                     client.Character.Save();
                     break;
-                case ".goname":
-                    Client targetClient = Server.Clients.FirstOrDefault(target => target.Character.Name == command[1]);
-                    if (targetClient != null && targetClient != client)
-                    {
-                        client.Character.Teleport(targetClient.Character.CurrentMapId, targetClient.Character.CellId);
-                    }
-                    else
-                    {
-                        BasicHandler.SendTextInformationMessage(client, TextInformationTypeEnum.TEXT_INFORMATION_ERROR, 16, new string[] { "Error", $"Can't found the character {command[1]} !" });
-                    }
-                    break;
                 case ".start":
                     client.Character.Teleport(client.Character.BreedData.StartMapId, client.Character.BreedData.StartDisposition.cellId);
                     break;
+                case ".goname":
+                    if (client.Account.Role >= GameHierarchyEnum.MODERATOR)
+                    {
+                        Client targetClient = Server.Clients.FirstOrDefault(target => target.Character.Name == command[1]);
+                        if (targetClient != null && targetClient != client)
+                        {
+                            client.Character.Teleport(targetClient.Character.CurrentMapId, targetClient.Character.CellId);
+                        }
+                        else
+                        {
+                            BasicHandler.SendTextInformationMessage(client, TextInformationTypeEnum.TEXT_INFORMATION_ERROR, 16, new string[] { "Error", $"Can't found the character {command[1]} !" });
+                        }
+                    }
+                    break;
                 case ".levelup":
-                    client.Character.LevelUp();
+                    if (client.Account.Role >= GameHierarchyEnum.GAMEMASTER_PADAWAN)
+                    {
+                        client.Character.LevelUp();
+                    }
                     break;
                 default:
                     BasicHandler.SendTextInformationMessage(client, TextInformationTypeEnum.TEXT_INFORMATION_ERROR, 16, new string[] { "Error", $"Command {command[0]} not found !" });
