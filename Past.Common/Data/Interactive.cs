0eb98ecd188f9f49f24f789fdb8da7052b6684fe
skeezr <skeeezr@hotmail.com>
2017-01-16 23:20:35 +0100

    add zaap interactive

---
+++ Past.Common/Data/Interactive.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Past.Protocol.Types;
+
+namespace Past.Common.Data
+{
+    public class Interactive
+    {
+        public static Dictionary<int, InteractiveElement[]> Interactives = new Dictionary<int, InteractiveElement[]>();
+
+        public static void Initialize() //Only zaaps & zaapis for the moment
+        {
+            Interactives.Add(2323, new[] { new InteractiveElement(433973, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(139265, new[] { new InteractiveElement(433974, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(21761028, new[] { new InteractiveElement(433975, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(21760512, new[] { new InteractiveElement(435662, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(16778242, new[] { new InteractiveElement(437119, new short[] { 44, 114 }, new short[0]) }); //duty free
+            Interactives.Add(154642, new[] { new InteractiveElement(443686, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(800, new[] { new InteractiveElement(57531, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(138543, new[] { new InteractiveElement(57534, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(147768, new[] { new InteractiveElement(260872, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(141588, new[] { new InteractiveElement(437118, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(143372, new[] { new InteractiveElement(437117, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(144419, new[] { new InteractiveElement(58211, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(133896, new[] { new InteractiveElement(443366, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(17932, new[] { new InteractiveElement(458282, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(13060, new[] { new InteractiveElement(1254, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(12054, new[] { new InteractiveElement(149940, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(8991, new[] { new InteractiveElement(1246, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(13605, new[] { new InteractiveElement(1261, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(15654, new[] { new InteractiveElement(60101, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(15153, new[] { new InteractiveElement(159227, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(14419207, new[] { new InteractiveElement(52836, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(20973313, new[] { new InteractiveElement(437121, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(28050436, new[] { new InteractiveElement(435664, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(131608, new[] { new InteractiveElement(57527, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(5142, new[] { new InteractiveElement(57554, new short[] { 44, 114 }, new short[0]) });
+            Interactives.Add(2567, new[] { new InteractiveElement(57272, new short[] { 44, 114 }, new short[0]) });
+        }
+    }
+}
