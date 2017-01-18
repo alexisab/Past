6ce01401ff22bc02c10e8b1047346519661bd91b
skeezr <skeeezr@gmail.com>
2017-01-18 13:46:29 +0100

    test

--- Past.Game/Network/Handlers/Interactive/InteractiveHandler.cs
+++ Past.Game/Network/Handlers/Interactive/InteractiveHandler.cs
@@ -1,23 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Past.Protocol.Enums;
 using Past.Protocol.Messages;
 
 namespace Past.Game.Network.Handlers.Interactive
 {
-    public class InteractiveHandler //InteractiveUseRequestMessage
+    public class InteractiveHandler
     {
         public static void HandleInteractiveUseRequestMessage(GameClient client, InteractiveUseRequestMessage message)
         {
             client.Character.CurrentMap.Send(new InteractiveUsedMessage(client.Character.Id, message.elemId, message.skillId, 0));
+            switch (message.skillId)
+            {
+                case 44:
+                    client.Character.SetSpawnMap(client.Character.Map.Id);
+                    break;
+                case 114:
+                    IEnumerable<int> zaaps = from interactive in Common.Data.Interactive.Interactives
+                        from interactiveElement in interactive.Value
+                        where interactiveElement.enabledSkillIds.SequenceEqual(new short[] {44, 114})
+                        select interactive.Key;
+                    IEnumerable<short> zaapsSubAreaIds = from interactive in Common.Data.Interactive.Interactives
+                        join map in Common.Data.Map.Maps on interactive.Key equals map.Id
+                        select (short) map.SubAreaId;
+                    client.Send(new ZaapListMessage(0, zaaps.ToArray(), zaapsSubAreaIds.ToArray(), new short[zaaps.Count()], client.Character.GetSpawnMap()));
+                    break;
+            }
             client.Send(new InteractiveUseEndedMessage(message.elemId, message.skillId));
-            var shit = from entry in Common.Data.Interactive.Interactives
-                       join xxx in Common.Data.Map.Maps on entry.Key equals xxx.Id
-                       select (short)xxx.SubAreaId;
+        }
 
-            client.Send(new ZaapListMessage(0, Common.Data.Interactive.Interactives.Keys.ToArray(), shit.ToArray(), new short[Common.Data.Interactive.Interactives.Count], 2323));
-           
+        public static void HandleTeleportRequestMessage(GameClient client, TeleportRequestMessage message)
+        {
         }
     }
 }
